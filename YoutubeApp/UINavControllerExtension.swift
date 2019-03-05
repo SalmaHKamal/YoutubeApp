b4e1341952e36e48c8ced0f1531a4073febca21c
Salma Hassan <salmahassan087@gmail.com>
2019-03-06 01:15:08 +0200

    created video cells at home page

---
+++ YoutubeApp/UINavControllerExtension.swift
@@ -0,0 +1,16 @@
+//
+//  UINavControllerExtension.swift
+//  YoutubeApp
+//
+//  Created by Salma Hassan on 3/5/19.
+//  Copyright © 2019 Salma Hassan. All rights reserved.
+//
+
+import UIKit
+
+extension UINavigationController {
+    
+    override open var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
